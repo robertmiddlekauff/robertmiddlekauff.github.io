d138a035d676352147cbb3d31509de4924ef0911
Robert Middlekauff <robert_middlekauff@mckinsey.com>
2023-01-31 10:28:41 -0600

    set up nav and header

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,70 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
+import { Tabs, Tab, AppBar, Container, Toolbar, Typography, Box, IconButton, MenuItem, Menu, Button, Tooltip } from '@mui/material';
 
 function App() {
+
+  const routes=['/', '/about']
+  const pages=[{title: "Home", route: "/"}, {title: "About", route: '/about'}]
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-        <div>
-        <BrowserRouter>
-        <NavBar/>
-          <Routes >
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-          </BrowserRouter>
-        </div>
+    <BrowserRouter>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+            {pages.map((page) => (
+              <Button
+                key={page.title}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+            <NavLink to={page.route}>{page.title}</NavLink>
+              </Button>
+            ))}
+          </Box>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Robert Middlekauff
+          </Typography>
+        </Toolbar>
+        
+      </Container>
+    </AppBar>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- my-app/src/components/NavBar.tsx
+++ my-app/src/components/NavBar.tsx
@@ -3,12 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return(
-		<nav className='ui raised very padded segment' >
-			<div className='ui right floated header'>
-				<button><NavLink to="/">Home</NavLink></button>
-				<button><NavLink to="/about">About</NavLink></button>
-			</div>
-		</nav>
+		<div>test</div>
 	)
 }
 
